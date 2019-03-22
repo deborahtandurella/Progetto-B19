15e4e6b94ec9fa877cc88d057099196aaf4662e4
JabJava <leonardo.montebugnoli01@universitadipavia.it>
2019-03-22 19:23:06 +0100

    Aggiunta classe main contiene una prima inizializzazione del server

--- ServerSide/src/Main.java
+++ ServerSide/src/Main.java
@@ -1,10 +1,15 @@
+//NB classe tester non necessaria javadoc
 public class Main {
+
+	//Membro statico del main in modo tale che da ogni punto del programma se c'è bisogno ci si può agganciare per interagire
+	private static Server s;
+
 	public static void main(String[] args) {
 
 		//Inizializzo un nuovo server
 		//NB: all'avvio del programma bisogna passare 3 argomenti (porta, host, numero massimo clients)
 		//TODO: magari trovare una soluzione migliore anzichè passarlo come argomento del programma
-		Server s = new Server(Integer.parseInt(args[0]),args[1],Integer.parseInt(args[2]));
+		s = new Server(Integer.parseInt(args[0]),args[1],Integer.parseInt(args[2]));
 
 		s.start();
 

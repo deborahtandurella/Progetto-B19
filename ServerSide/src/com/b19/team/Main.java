6a884e7935a64c5fc0edc13178295bfd84408409
JabJava <leonardo.montebugnoli01@universitadipavia.it>
2019-04-02 15:31:19 +0200

    Bug fixing

--- ServerSide/src/com/b19/team/Main.java
+++ ServerSide/src/com/b19/team/Main.java
@@ -1,27 +1,24 @@
 package com.b19.team;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 //NB classe tester non necessaria javadoc
 public class Main {
 
 	//Membro statico del main in modo tale che da ogni punto del programma se c'è bisogno ci si può agganciare per interagire
 	private static Server s;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 
 		//Inizializzo un nuovo server
 		//NB: all'avvio del programma bisogna passare 3 argomenti (porta, host, numero massimo clients)
 		//TODO: magari trovare una soluzione migliore anzichè passarlo come argomento del programma
 
-
-		byte[] host = new byte[4];
-
-		host[0] = Byte.valueOf(args[1]);
-		host[1] = Byte.valueOf(args[2]);
-		host[2] = Byte.valueOf(args[3]);
-		host[3] = Byte.valueOf(args[4]);
+		InetAddress address = InetAddress.getByName(args[1]);
 
 
-		s = new Server(Integer.parseInt(args[0]),host,Integer.parseInt(args[2]));
+		s = new Server(Integer.parseInt(args[0]),address,Integer.parseInt(args[2]));
 
 		s.start();
 

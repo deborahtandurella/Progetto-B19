d414b1dd6a85ef9a142fcb2f5f94584544d3dcda
alerossi97 <alessandro.rossi09@universitadipavia.it>
2019-04-02 17:08:44 +0200

    Modifiche javafx

--- ClientSide/src/Main.java
+++ ClientSide/src/Main.java
@@ -1,7 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        Client client =  new Client();
 
-        client.avviaClient();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        /**Client client =  new Client();
+
+        client.avviaClient();*/
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuStage.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+
 }

c4eb605ac567731767988c8860ef47bdb37b3374
alerossi97 <alessandro.rossi09@universitadipavia.it>
2019-04-02 17:11:01 +0200

    Controller javafx per menu

---
+++ ClientSide/src/MenuController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    Button BTN_PLAY;
+
+    @FXML
+    public void ActionPlay(){
+        System.out.println("PLAY");
+    }
+
+}

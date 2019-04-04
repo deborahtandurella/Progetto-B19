fd6eb027b7c6e13269655901b61aec667bcc95bd
JabJava <leonardo.montebugnoli01@universitadipavia.it>
2019-04-05 00:32:50 +0100

    Aggiunto enumeratore per lo stato del player

---
+++ ServerSide/src/com/b19/team/PlayerState.java
@@ -0,0 +1,12 @@
+package com.b19.team;
+
+/**
+ * Questo enum gestisce lo stato del giocatore, IDLE significa che il giocatore è connesso ma non ha inviato un username
+ * mentre CONNECTED significa che il giocatore è connesso e ha un username quindi è pronto per giocare
+ */
+public enum PlayerState {
+
+	IDLE,
+	CONNECTED
+
+}

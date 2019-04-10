57a8ad65931d7e0aade61591bab2df59f4e040d9
JabJava <leonardo.montebugnoli01@universitadipavia.it>
2019-04-10 22:29:54 +0100

    Better scene transition with slide effect

--- ClientSide/src/MenuController.java
+++ ClientSide/src/MenuController.java
@@ -1,3 +1,7 @@
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,8 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,17 +36,42 @@ public class MenuController {
     ImageView imageView;
 
     @FXML
+    StackPane parentPane;
+
+    @FXML
     public void ActionPlay(ActionEvent event) throws IOException {
         PlayerController.startGame(txt_username.getText());
 
         //metodo che cambia la scena
         Parent cartellaParent = FXMLLoader.load(getClass().getResource("Cartella.fxml"));
 
-        Scene scene = new Scene(cartellaParent);
+        AnchorPane next = (AnchorPane) cartellaParent;
 
-        Stage stage = (Stage) BTN_PLAY.getScene().getWindow();
-        stage.setScene(scene);
+        next.setTranslateX(parentPane.getScene().getWidth());
+        next.toFront();
+        parentPane.getChildren().add(next);
 
+
+        KeyValue kv = new KeyValue(next.translateXProperty(),0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1),kv);
+
+        Timeline timeline = new Timeline();
+
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(event1 -> removeAnchor("#menuAnchor"));
+        timeline.play();
+
+
+
+        //Scene transition
+
+    }
+
+    private void removeAnchor(String name){
+        AnchorPane p = (AnchorPane) parentPane.getScene().lookup(name);
+        if(p != null){
+            parentPane.getChildren().remove(p);
+        }
     }
 
 }

b054ae947790ba911b01a32c662d2b4e63aa91f7
JabJava <leonardo.montebugnoli01@universitadipavia.it>
2019-04-18 12:11:14 +0100

    Aggiunte alla classe che memorizza la cartella

--- ClientSide/src/com/Game/Cartella.java
+++ ClientSide/src/com/Game/Cartella.java
@@ -5,13 +5,32 @@ import java.util.ArrayList;
 public class Cartella {
 
 	private Integer[] numeri;
+	private ArrayList<Integer> winningRow;
+	private boolean validCard;
 
 	public Cartella(ArrayList<Integer> numeri) {
 		this.numeri = numeri.toArray(new Integer[0]);
+		this.winningRow = new ArrayList<>();
+		this.validCard = true;
 	}
 
 	public Integer[] getNumeri(){
 		return numeri;
 	}
 
+	public boolean isValidWinningRow(int r){
+		return !winningRow.contains(r);
+	}
+
+	public void addWinningRow(int r){
+		winningRow.add(r);
+	}
+
+	public void setInvalid(){
+		validCard = false;
+	}
+
+	public boolean isValidCard() {
+		return validCard;
+	}
 }

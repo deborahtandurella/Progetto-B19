dd31bdcc1f77c8ad9d7933774fd6ad7c5a548be8
alerossi97 <alessandro.rossi09@universitadipavia.it>
2019-05-09 16:46:51 +0200

    Modifiche GUI by Matteo Poire

--- ClientSide/src/Main.java
+++ ClientSide/src/Main.java
@@ -1,7 +1,10 @@
 import com.GUI.CartellaComponent;
+import com.GUI.controllers.StartingInterfaceController;
 import com.Game.Cartella;
 import com.Game.CartellaFactory;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,21 +23,30 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("resources/MenuStage.fxml"));
 
-        Integer[] num = CartellaFactory.createCartella().getNumeri();
+        //Integer[] num = CartellaFactory.createCartella().getNumeri();
 
 
 
 
-        CartellaComponent cartella1 = new CartellaComponent();
-        cartella1.setNumeri(num);
-        Pane root = new Pane();
+        //CartellaComponent cartella1 = new CartellaComponent();
+        //cartella1.setNumeri(num);
+        //Pane root = new Pane();
 
 
 
-        root.getChildren().add(cartella1);
-        Scene scene = new Scene(root);
+        //root.getChildren().add(cartella1);
+        //Scene scene = new Scene(root);
 
-        scene.getStylesheets().add(this.getClass().getResource("resources/Style.CSS").toExternalForm() );
+        //scene.getStylesheets().add(this.getClass().getResource("resources/Style.CSS").toExternalForm() );
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/StartingInterface.fxml"));
+
+
+        Parent parent = loader.load();
+        Scene scene = new Scene(parent,600,400);
+
+
 
         primaryStage.setScene(scene);
         primaryStage.show();

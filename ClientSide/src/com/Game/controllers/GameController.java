b1a4f0b4476babf4c5212dac23b31770f5aa04a4
JabJava <leonardo.montebugnoli01@universitadipavia.it>
2019-05-13 18:29:42 +0200

    Termine prima iterazione

--- ClientSide/src/com/Game/controllers/GameController.java
+++ ClientSide/src/com/Game/controllers/GameController.java
@@ -4,22 +4,60 @@ import com.Game.CallEnum;
 import com.Game.Cartella;
 import com.Game.Tomboliere;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GameController {
 
 	private Player p;
 	private Tomboliere t;
+	private Thread estrattore;
+	private int n;
 
-	public GameController(String playerName) {
+	public GameController(String playerName, int n) {
 
 		p = new Player(playerName);
 		t = new Tomboliere();
-		p.addCartella(t.getCartella());
+		for (int i = 0; i < n; i++) {
+			p.addCartella(t.getCartella());
+		}
+
+		this.n = n;
+
+
+
+
+	}
+
+	public void startExtraction(Runnable updateFunction) {
+		estrattore = new Thread(() -> {
+			while (true){
+
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+
+				int numero = estraiNumero();
+				updateFunction.run();
+			}
+		});
+
+		estrattore.start();
+	}
+
+	public void stopExtractions(){
+		estrattore.interrupt();
+		estrattore.stop();
 	}
 
 
-	public Integer[] getCartella() {
-		return p.getCartella().getNumeri();
+	public Cartella getCartella(int index) {
+		return p.getCartella(index);
 	}
+	public Integer[] getCartellaAsArray(int index) {return p.getCartella(index).getNumeri();}
 
 	public boolean buttonControl(CallEnum callEnum, Cartella cartella) {
 		return t.checkCall(callEnum,cartella);
@@ -29,4 +67,14 @@ public class GameController {
 		return t.getNumber();
 	}
 
+	public ArrayList<Integer> getExtractions() {
+
+
+
+		return t.getExtractions();
+	}
+
+	public int getCartellaCount() {
+		return n;
+	}
 }
--- ClientSide/src/com/Game/controllers/Player.java
+++ ClientSide/src/com/Game/controllers/Player.java
@@ -2,24 +2,26 @@ package com.Game.controllers;
 
 import com.Game.Cartella;
 
+import java.util.ArrayList;
+
 public class Player {
 
 	private String username;
-	private Cartella c;
+	private ArrayList<Cartella> cartelle;
 
 
 
 	public Player(String username) {
 		this.username = username;
-
+		cartelle = new ArrayList<>();
 	}
 
 	public void addCartella(Cartella cartella) {
-		c = cartella;
+		cartelle.add(cartella);
 	}
 
-	public Cartella getCartella() {
-		return c;
+	public Cartella getCartella(int index) {
+		return cartelle.get(index);
 	}
 
 	public String getUsername() {

6fd1a69e96417c2e70c0613b87f9207c45b85d92
AlessandroPasotti <alessandropasotti97@gmail.com>
2019-07-14 18:37:49 +0200

    Aggiunta commenti

--- ClientSide/src/com/Game/CartellaFactory.java
+++ ClientSide/src/com/Game/CartellaFactory.java
@@ -3,40 +3,59 @@ package com.Game;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Generate rows and columns of a table and assign the numbers
+ */
 public class CartellaFactory {
 
-
+	/**
+	 * Create the card
+	 *
+	 * @return c the created card
+	 */
 	public static Cartella createCartella(){
 
+		//Numbers of a card
 		ArrayList<Integer> numeri = new ArrayList<>();
 
-
+		//Create the first row of the card
 		ArrayList<Integer> tmpNum = createRow(numeri);
 		numeri.addAll(tmpNum);
 
+		//Create the second row of the card
 		tmpNum = createRow(numeri);
 		numeri.addAll(tmpNum);
 
+		//Create the third row of the card
 		tmpNum = createRow(numeri);
 		numeri.addAll(tmpNum);
 
-
+		//Create a card and assign the numbers
 		Cartella c = new Cartella(numeri);
 		return c;
 	}
 
+	/**
+	 * Set the row of a table
+	 *
+	 * @param oldNum numbers that have been assigned previously to a card
+	 *
+	 * @return numeri numbers in the row
+	 */
 	private static ArrayList<Integer> createRow(ArrayList<Integer> oldNum){
+
+		//Random number generator
 		Random r = new Random();
 
+		//Numbers in the row
 		ArrayList<Integer> numeri = new ArrayList<>();
 
+		//Assign the numbers to the arraylist
 		for (int i = 0; i < 9; i++) {
 
 			int n;
+
 			while(true) {
-
-
-
 				if (i == 0) {
 					n = r.nextInt(8) + 1;
 				} else {
@@ -48,12 +67,13 @@ public class CartellaFactory {
 				}
 			}
 
-
 			numeri.add(n);
 		}
 
+		//Counter
 		int a = 0;
 
+		//Assign the numbers to the row
 		while(a <4){
 			int n = r.nextInt(numeri.size());
 			if(numeri.get(n) != 0) {
